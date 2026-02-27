c9138c958bae9af27d4abcafe811c07b3bef2052
harshLearningSomeDev <kumarharsh620730@gmail.com>
2026-02-28 01:26:32 +0530

    Day 1: Frontend Vite app created

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(5000, () => console.log("Server started on port 5000"));
